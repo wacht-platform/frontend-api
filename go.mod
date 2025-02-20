32e8edc010feb4316492d0bb7487a894b1c17a9d
Saurav Singh <snipextt@gmail.com>
2025-02-20 10:35:33 +0530

    wip

--- go.mod
+++ go.mod
@@ -29,7 +29,7 @@ require (
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-sql-driver/mysql v1.8.1 // indirect
-	github.com/goccy/go-json v0.10.3 // indirect
+	github.com/goccy/go-json v0.10.5 // indirect
 	github.com/gofiber/storage/redis/v3 v3.1.3 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 
+	"github.com/goccy/go-json"
 	"github.com/gofiber/fiber/v2"
 	"github.com/ilabs/wacht-fe/config"
 	"github.com/ilabs/wacht-fe/database"
@@ -17,11 +18,14 @@ func main() {
 		log.Fatal("Error connecting to database: ", err)
 	}
 
-	if err = database.AutoMigratePg(); err != nil {
-		log.Fatal("Error migrating database: ", err)
-	}
+	// if err = database.AutoMigratePg(); err != nil {
+	// 	log.Fatal("Error migrating database: ", err)
+	// }
 
-	app := fiber.New()
+	app := fiber.New(fiber.Config{
+		JSONEncoder: json.Marshal,
+		JSONDecoder: json.Unmarshal,
+	})
 	router.Setup(app)
 
 	log.Fatal(app.Listen(":3000"))
