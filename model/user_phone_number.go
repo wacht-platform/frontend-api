148693426b2c520b5fe5fd33de93f9ebba217392
Saurav Singh <snipextt@gmail.com>
2025-05-24 14:57:43 +0530

    wip

--- model/user_phone_number.go
+++ model/user_phone_number.go
@@ -4,9 +4,11 @@ import "time"
 
 type UserPhoneNumber struct {
 	Model
-	UserID                uint64    `json:"-"`
-	PhoneNumber           string    `json:"phone_number"              gorm:"not null"`
-	CanUseForSecondFactor bool      `json:"can_use_for_second_factor" gorm:"not null"`
-	Verified              bool      `json:"verified"                  gorm:"not null"`
-	VerifiedAt            time.Time `json:"verified_at"`
+	UserID                uint64     `json:"-"`
+	PhoneNumber           string     `json:"phone_number"              gorm:"not null"`
+	CanUseForSecondFactor bool       `json:"can_use_for_second_factor" gorm:"not null"`
+	Verified              bool       `json:"verified"                  gorm:"not null"`
+	VerifiedAt            time.Time  `json:"verified_at"`
+	DeploymentID          uint64     `json:"-"`
+	Deployment            Deployment `json:"-" gorm:"foreignkey:DeploymentID"`
 }
