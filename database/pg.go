4a7f4f8aa762358c0da96d7b4a2b8b67009284ca
Saurav Singh <snipextt@gmail.com>
2025-05-26 11:26:40 +0530

    add waitlist

--- database/pg.go
+++ database/pg.go
@@ -72,6 +72,7 @@ func AutoMigratePg() error {
 		&model.DeploymentJwtTemplate{},
 		&model.DeploymentEmailTemplate{},
 		&model.DeploymentSmsTemplate{},
+		&model.DeploymentInvitation{},
 		&model.DeploymentWaitlistUser{},
 	)
 }
