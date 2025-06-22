c3e28d8bf995868026ed15abb3b44adfc3997da1
Saurav Singh <snipextt@gmail.com>
2025-06-22 13:07:37 +0530

    fix

--- utils/session.go
+++ utils/session.go
@@ -1,7 +1,6 @@
 package utils
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -12,13 +11,7 @@ import (
 	"github.com/ilabs/wacht-fe/model"
 )
 
-type SessionQueryResult struct {
-	model.Session
-	SigninAttempts []byte `gorm:"column:signin_attempts"`
-	Signins        []byte `gorm:"column:signins"`
-	SignupAttempts []byte `gorm:"column:signup_attempts"`
-	ActiveSignin   []byte `gorm:"column:active_signin"`
-}
+
 
 func GetSessionByID(sessionID uint64) (*model.Session, error) {
 	cacheKey := "session:" + strconv.FormatUint(sessionID, 10)
@@ -30,203 +23,101 @@ func GetSessionByID(sessionID uint64) (*model.Session, error) {
 		}
 	}
 
-	queryResult := new(SessionQueryResult)
-	rawSQL := `
-		WITH user_email_addresses_agg AS (
-			SELECT user_id, json_agg(json_build_object(
-				'id', uea.id::text,
-				'created_at', uea.created_at,
-				'updated_at', uea.updated_at,
-				'email', uea.email,
-				'is_primary', uea.is_primary,
-				'verified', uea.verified,
-				'verified_at', uea.verified_at,
-				'verification_strategy', uea.verification_strategy
-			)) AS user_email_addresses
-			FROM user_email_addresses uea
-			GROUP BY user_id
-		),
-		user_phone_numbers_agg AS (
-			SELECT user_id, json_agg(json_build_object(
-				'id', upn.id::text,
-				'created_at', upn.created_at,
-				'updated_at', upn.updated_at,
-				'phone_number', upn.phone_number,
-				'is_primary', upn.is_primary,
-				'verified', upn.verified,
-				'verified_at', upn.verified_at,
-				'verification_strategy', upn.verification_strategy
-			)) AS user_phone_numbers
-			FROM user_phone_numbers upn
-			GROUP BY user_id
-		),
-		social_connections_agg AS (
-			SELECT user_id, json_agg(json_build_object(
-				'id', sc.id::text,
-				'created_at', sc.created_at,
-				'updated_at', sc.updated_at,
-				'provider', sc.provider,
-				'provider_user_id', sc.provider_user_id,
-				'email', sc.email,
-				'name', sc.name,
-				'avatar_url', sc.avatar_url
-			)) AS social_connections
-			FROM social_connections sc
-			GROUP BY user_id
-		),
-		users_agg AS (
-			SELECT
-				u.id,
-				json_build_object(
-					'id', u.id::text,
-					'created_at', u.created_at,
-					'updated_at', u.updated_at,
-					'first_name', u.first_name,
-					'has_profile_picture', u.has_profile_picture,
-					'profile_picture_url', u.profile_picture_url,
-					'last_name', u.last_name,
-					'username', u.username,
-					'availability', u.availability,
-					'last_password_reset_at', u.last_password_reset_at,
-					'schema_version', u.schema_version,
-					'disabled', u.disabled,
-					'primary_email_address_id', u.primary_email_address_id::text,
-					'primary_phone_number_id', u.primary_phone_number_id::text,
-					'second_factor_policy', u.second_factor_policy,
-					'active_organization_membership_id', u.active_organization_membership_id::text,
-					'active_workspace_membership_id', u.active_workspace_membership_id::text,
-					'public_metadata', u.public_metadata,
-					'backup_codes_generated', u.backup_codes_generated,
-					'user_email_addresses', uea.user_email_addresses,
-					'user_phone_numbers', upn.user_phone_numbers,
-					'social_connections', sc.social_connections
-				) as user
-			FROM users u
-			LEFT JOIN user_email_addresses_agg uea ON u.id = uea.user_id
-			LEFT JOIN user_phone_numbers_agg upn ON u.id = upn.user_id
-			LEFT JOIN social_connections_agg sc ON u.id = sc.user_id
-		),
-		signins_agg AS (
-			SELECT
-				s.id as session_id,
-				json_agg(json_build_object(
-					'id', si.id::text,
-					'created_at', si.created_at,
-					'updated_at', si.updated_at,
-					'session_id', si.session_id::text,
-					'user_id', si.user_id::text,
-					'active_organization_membership_id', si.active_organization_membership_id::text,
-					'active_workspace_membership_id', si.active_workspace_membership_id::text,
-					'expires_at', si.expires_at,
-					'last_active_at', si.last_active_at,
-					'ip_address', si.ip_address,
-					'browser', si.browser,
-					'device', si.device,
-					'city', si.city,
-					'region', si.region,
-					'region_code', si.region_code,
-					'country', si.country,
-					'country_code', si.country_code,
-					'user', ua.user
-				)) as signins
-			FROM sessions s
-			JOIN signins si ON s.id = si.session_id
-			LEFT JOIN users_agg ua ON si.user_id = ua.id
-			GROUP BY s.id
-		),
-		active_signin_agg AS (
-			SELECT
-				s.id as session_id,
-				json_build_object(
-					'id', si.id::text,
-					'created_at', si.created_at,
-					'updated_at', si.updated_at,
-					'session_id', si.session_id::text,
-					'user_id', si.user_id::text,
-					'active_organization_membership_id', si.active_organization_membership_id::text,
-					'active_workspace_membership_id', si.active_workspace_membership_id::text,
-					'expires_at', si.expires_at,
-					'last_active_at', si.last_active_at,
-					'ip_address', si.ip_address,
-					'browser', si.browser,
-					'device', si.device,
-					'city', si.city,
-					'region', si.region,
-					'region_code', si.region_code,
-					'country', si.country,
-					'country_code', si.country_code,
-					'user', ua.user
-				) as active_signin
-			FROM sessions s
-			JOIN signins si ON s.active_signin_id = si.id
-			LEFT JOIN users_agg ua ON si.user_id = ua.id
-		),
-		signin_attempts_agg AS (
-			SELECT session_id, json_agg(json_build_object(
-				'id', sa.id::text,
-				'created_at', sa.created_at,
-				'updated_at', sa.updated_at,
-				'session_id', sa.session_id::text,
-				'method', sa.method,
-				'sso_provider', sa.sso_provider,
-				'expires_at', sa.expires_at,
-				'current_step', sa.current_step,
-				'remaining_steps', sa.remaining_steps,
-				'completed', sa.completed,
-				'errored', sa.errored,
-				'errors', sa.errors,
-				'requires_completion', sa.requires_completion,
-				'missing_fields', sa.missing_fields,
-				'required_fields', sa.required_fields
-			)) as signin_attempts
-			FROM sign_in_attempts sa
-			GROUP BY session_id
-		),
-		signup_attempts_agg AS (
-			SELECT session_id, json_agg(json_build_object(
-				'id', sua.id::text,
-				'created_at', sua.created_at,
-				'updated_at', sua.updated_at,
-				'session_id', sua.session_id::text,
-				'first_name', sua.first_name,
-				'last_name', sua.last_name,
-				'email', sua.email,
-				'username', sua.username,
-				'phone_number', sua.phone_number,
-				'required_fields', sua.required_fields,
-				'missing_fields', sua.missing_fields,
-				'current_step', sua.current_step,
-				'remaining_steps', sua.remaining_steps,
-				'sso_provider', sua.sso_provider,
-				'is_oauth_signup', sua.is_oauth_signup
-			)) as signup_attempts
-			FROM signup_attempts sua
-			GROUP BY session_id
-		)
-		SELECT
-			s.*,
-			sa.signin_attempts,
-			si.signins,
-			sua.signup_attempts,
-			asi.active_signin
-		FROM sessions s
-		LEFT JOIN signin_attempts_agg sa ON s.id = sa.session_id
-		LEFT JOIN signins_agg si ON s.id = si.session_id
-		LEFT JOIN signup_attempts_agg sua ON s.id = sua.session_id
-		LEFT JOIN active_signin_agg asi ON s.id = asi.session_id
-		WHERE s.id = ? AND s.deleted_at IS NULL
-	`
-	err = database.Connection.Raw(rawSQL, sessionID).Scan(queryResult).Error
+	session := new(model.Session)
 
-	if err != nil || queryResult.ID == 0 {
+	// Single query using JOINs to get session with active signin and all required data
+	err = database.Connection.
+		Joins("LEFT JOIN signins active_signin ON sessions.active_signin_id = active_signin.id").
+		Joins("LEFT JOIN users active_user ON active_signin.user_id = active_user.id").
+		Joins("LEFT JOIN organization_memberships active_org_mem ON active_signin.active_organization_membership_id = active_org_mem.id").
+		Joins("LEFT JOIN workspace_memberships active_ws_mem ON active_signin.active_workspace_membership_id = active_ws_mem.id").
+		Select(`
+			sessions.*,
+			active_signin.id as "ActiveSignin__id",
+			active_signin.created_at as "ActiveSignin__created_at",
+			active_signin.updated_at as "ActiveSignin__updated_at",
+			active_signin.user_id as "ActiveSignin__user_id",
+			active_signin.session_id as "ActiveSignin__session_id",
+			active_signin.active_organization_membership_id as "ActiveSignin__active_organization_membership_id",
+			active_signin.active_workspace_membership_id as "ActiveSignin__active_workspace_membership_id",
+			active_signin.expires_at as "ActiveSignin__expires_at",
+			active_signin.last_active_at as "ActiveSignin__last_active_at",
+			active_signin.ip_address as "ActiveSignin__ip_address",
+			active_signin.browser as "ActiveSignin__browser",
+			active_signin.device as "ActiveSignin__device",
+			active_signin.city as "ActiveSignin__city",
+			active_signin.region as "ActiveSignin__region",
+			active_signin.region_code as "ActiveSignin__region_code",
+			active_signin.country as "ActiveSignin__country",
+			active_signin.country_code as "ActiveSignin__country_code",
+			active_user.id as "ActiveSignin__User__id",
+			active_user.created_at as "ActiveSignin__User__created_at",
+			active_user.updated_at as "ActiveSignin__User__updated_at",
+			active_user.first_name as "ActiveSignin__User__first_name",
+			active_user.last_name as "ActiveSignin__User__last_name",
+			active_user.username as "ActiveSignin__User__username",
+			active_user.has_profile_picture as "ActiveSignin__User__has_profile_picture",
+			active_user.profile_picture_url as "ActiveSignin__User__profile_picture_url",
+			active_user.availability as "ActiveSignin__User__availability",
+			active_user.last_password_reset_at as "ActiveSignin__User__last_password_reset_at",
+			active_user.schema_version as "ActiveSignin__User__schema_version",
+			active_user.disabled as "ActiveSignin__User__disabled",
+			active_user.primary_email_address_id as "ActiveSignin__User__primary_email_address_id",
+			active_user.primary_phone_number_id as "ActiveSignin__User__primary_phone_number_id",
+			active_user.second_factor_policy as "ActiveSignin__User__second_factor_policy",
+			active_user.active_organization_membership_id as "ActiveSignin__User__active_organization_membership_id",
+			active_user.active_workspace_membership_id as "ActiveSignin__User__active_workspace_membership_id",
+			active_user.public_metadata as "ActiveSignin__User__public_metadata",
+			active_user.backup_codes_generated as "ActiveSignin__User__backup_codes_generated",
+			active_org_mem.id as "ActiveSignin__ActiveOrganizationMembership__id",
+			active_org_mem.organization_id as "ActiveSignin__ActiveOrganizationMembership__organization_id",
+			active_org_mem.user_id as "ActiveSignin__ActiveOrganizationMembership__user_id",
+			active_ws_mem.id as "ActiveSignin__ActiveWorkspaceMembership__id",
+			active_ws_mem.workspace_id as "ActiveSignin__ActiveWorkspaceMembership__workspace_id",
+			active_ws_mem.user_id as "ActiveSignin__ActiveWorkspaceMembership__user_id",
+			active_ws_mem.organization_membership_id as "ActiveSignin__ActiveWorkspaceMembership__organization_membership_id"
+		`).
+		Where("sessions.id = ? AND sessions.deleted_at IS NULL", sessionID).
+		First(session).Error
+
+	if err != nil {
 		return nil, fmt.Errorf("session not found")
 	}
 
-	session := &queryResult.Session
-	json.Unmarshal(queryResult.SigninAttempts, &session.SigninAttempts)
-	json.Unmarshal(queryResult.Signins, &session.Signins)
-	json.Unmarshal(queryResult.SignupAttempts, &session.SignupAttempts)
-	json.Unmarshal(queryResult.ActiveSignin, &session.ActiveSignin)
+	database.Connection.Where("session_id = ?", sessionID).Find(&session.SigninAttempts)
+
+	database.Connection.Where("session_id = ?", sessionID).Find(&session.SignupAttempts)
+
+	database.Connection.
+		Joins("LEFT JOIN users ON signins.user_id = users.id").
+		Where("signins.session_id = ?", sessionID).
+		Find(&session.Signins)
+
+	if session.ActiveSignin != nil && session.ActiveSignin.ActiveOrganizationMembershipID != nil {
+		var orgRoles []*model.OrganizationRole
+		database.Connection.
+			Joins("JOIN organization_membership_roles ON organization_roles.id = organization_membership_roles.organization_role_id").
+			Where("organization_membership_roles.organization_membership_id = ?", *session.ActiveSignin.ActiveOrganizationMembershipID).
+			Find(&orgRoles)
+
+		if session.ActiveSignin.ActiveOrganizationMembership == nil {
+			session.ActiveSignin.ActiveOrganizationMembership = &model.OrganizationMembership{}
+		}
+		session.ActiveSignin.ActiveOrganizationMembership.Roles = orgRoles
+	}
+
+	if session.ActiveSignin != nil && session.ActiveSignin.ActiveWorkspaceMembershipID != nil {
+		var wsRoles []*model.WorkspaceRole
+		database.Connection.
+			Joins("JOIN workspace_membership_roles ON workspace_roles.id = workspace_membership_roles.workspace_role_id").
+			Where("workspace_membership_roles.workspace_membership_id = ?", *session.ActiveSignin.ActiveWorkspaceMembershipID).
+			Find(&wsRoles)
+
+		if session.ActiveSignin.ActiveWorkspaceMembership == nil {
+			session.ActiveSignin.ActiveWorkspaceMembership = &model.WorkspaceMembership{}
+		}
+		session.ActiveSignin.ActiveWorkspaceMembership.Roles = wsRoles
+	}
 
 	go setSessionCache(*session)
 
