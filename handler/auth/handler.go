6a4c898db3346265a862217b9be3827a6774e957
Saurav Singh <snipextt@gmail.com>
2025-07-09 00:41:43 +0530

    Replace NewSignIn constructor with service CreateSignin method

--- handler/auth/handler.go
+++ handler/auth/handler.go
@@ -144,7 +144,7 @@ func (h *Handler) handleUsernameSignIn(c *fiber.Ctx, b SignInRequest, d model.De
 				return err
 			}
 
-			signIn := model.NewSignIn(session.ID, user.ID)
+			signIn := h.service.CreateSignin(user.ID, session.ID, c)
 			if err := tx.Create(signIn).Error; err != nil {
 				return err
 			}
@@ -267,7 +267,7 @@ func (h *Handler) handleEmailPasswordSignIn(c *fiber.Ctx, b SignInRequest, d mod
 				return err
 			}
 
-			signIn := model.NewSignIn(session.ID, email.User.ID)
+			signIn := h.service.CreateSignin(email.User.ID, session.ID, c)
 			if err := tx.Create(signIn).Error; err != nil {
 				return err
 			}
@@ -560,7 +560,7 @@ func (h *Handler) SignUp(c *fiber.Ctx) error {
 				return err
 			}
 
-			signIn := model.NewSignIn(session.ID, u.ID)
+			signIn := h.service.CreateSignin(u.ID, session.ID, c)
 			signIn.User = &u
 
 			if err := tx.Create(signIn).Error; err != nil {
@@ -837,7 +837,7 @@ func (h *Handler) SSOCallback(c *fiber.Ctx) error {
 			attempt.Available2FAMethods = datatypes.NewJSONSlice([]string{})
 			attempt.Completed = false
 		} else {
-			signIn := model.NewSignIn(session.ID, u.ID)
+			signIn := h.service.CreateSignin(u.ID, session.ID, c)
 
 			if err := tx.Create(&signIn).Error; err != nil {
 				return err
@@ -1333,7 +1333,7 @@ func (h *Handler) VerifyMagicLink(c *fiber.Ctx) error {
 			return handler.SendBadRequest(c, nil, err.Error(), handler.ErrCountryRestricted)
 		}
 
-		signIn := model.NewSignIn(session.ID, email.User.ID)
+		signIn := h.service.CreateSignin(email.User.ID, session.ID, c)
 		signIn.User = &email.User
 
 		err = database.Connection.Transaction(func(tx *gorm.DB) error {
@@ -1494,7 +1494,7 @@ func (h *Handler) AttemptVerification(c *fiber.Ctx) error {
 				if len(attempt.RemainingSteps) == 1 {
 					attempt.Completed = true
 					attempt.RemainingSteps = nil
-					signin = model.NewSignIn(session.ID, *email.UserID)
+					signin = h.service.CreateSignin(*email.UserID, session.ID, c)
 					signin.User = &email.User
 
 					session.Signins = append(session.Signins, *signin)
@@ -1593,7 +1593,7 @@ func (h *Handler) AttemptVerification(c *fiber.Ctx) error {
 				if len(attempt.RemainingSteps) == 1 {
 					attempt.Completed = true
 					attempt.RemainingSteps = nil
-					signin = model.NewSignIn(session.ID, phone.User.ID)
+					signin = h.service.CreateSignin(phone.User.ID, session.ID, c)
 					signin.User = &phone.User
 
 					session.Signins = append(session.Signins, *signin)
@@ -1759,7 +1759,7 @@ func (h *Handler) AttemptVerification(c *fiber.Ctx) error {
 			if len(attempt.RemainingSteps) == 1 {
 				attempt.Completed = true
 				attempt.RemainingSteps = nil
-				signin = model.NewSignIn(session.ID, user.ID)
+				signin = h.service.CreateSignin(user.ID, session.ID, c)
 				signin.User = &user
 
 				session.Signins = append(session.Signins, *signin)
@@ -1836,7 +1836,7 @@ func (h *Handler) AttemptVerification(c *fiber.Ctx) error {
 				return handler.SendBadRequest(c, nil, err.Error(), handler.ErrCountryRestricted)
 			}
 
-			signIn := model.NewSignIn(session.ID, user.ID)
+			signIn := h.service.CreateSignin(user.ID, session.ID, c)
 			signIn.User = user
 
 			if err := database.Connection.Transaction(func(tx *gorm.DB) error {
@@ -1985,7 +1985,7 @@ func (h *Handler) CompleteOAuthSignup(c *fiber.Ctx) error {
 			return handler.SendBadRequest(c, nil, err.Error(), handler.ErrCountryRestricted)
 		}
 
-		signIn := model.NewSignIn(session.ID, user.ID)
+		signIn := h.service.CreateSignin(user.ID, session.ID, c)
 		signIn.User = user
 
 		err = database.Connection.Transaction(func(tx *gorm.DB) error {
@@ -2096,7 +2096,7 @@ func (h *Handler) CompleteSignInProfile(c *fiber.Ctx) error {
 			return handler.SendBadRequest(c, nil, err.Error(), handler.ErrCountryRestricted)
 		}
 
-		signIn := model.NewSignIn(session.ID, user.ID)
+		signIn := h.service.CreateSignin(user.ID, session.ID, c)
 		signIn.User = &user
 
 		err := database.Connection.Transaction(func(tx *gorm.DB) error {
@@ -2337,7 +2337,7 @@ func (h *Handler) handleOAuthSignupCompletion(c *fiber.Ctx, attempt *model.Signu
 			return handler.SendBadRequest(c, nil, err.Error(), handler.ErrCountryRestricted)
 		}
 
-		signIn := model.NewSignIn(session.ID, user.ID)
+		signIn := h.service.CreateSignin(user.ID, session.ID, c)
 		signIn.User = user
 
 		err = database.Connection.Transaction(func(tx *gorm.DB) error {
@@ -2451,7 +2451,7 @@ func (h *Handler) handleSigninProfileCompletion(c *fiber.Ctx, attempt *model.Sig
 			return handler.SendBadRequest(c, nil, err.Error(), handler.ErrCountryRestricted)
 		}
 
-		signIn := model.NewSignIn(session.ID, user.ID)
+		signIn := h.service.CreateSignin(user.ID, session.ID, c)
 		signIn.User = &user
 
 		err := database.Connection.Transaction(func(tx *gorm.DB) error {
