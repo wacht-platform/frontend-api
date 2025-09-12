826aa68e5d6b2058731d3b48d2c64f121131be43
Saurav Singh <snipextt@gmail.com>
2025-09-12 17:22:07 +0530

    Update prelude.go

--- middleware/prelude.go
+++ middleware/prelude.go
@@ -27,8 +27,7 @@ const (
 )
 
 func SetRequestPrelude(c *fiber.Ctx) error {
-	// host := c.Hostname()
-	host := "dimwitted-axis-1.frontend-api.services"
+	host := c.Hostname()
 	path := c.Path()
 
 	if net.ParseIP(host) != nil {
